bd6a6c562ad09b747fcc085bdce6800d15666aae
BulTTaeng <60182208+Choi-JaeHyeok-21500749@users.noreply.github.com>
2022-03-15 18:50:39 +0900

    Create 15973_TwoBox.cpp

---
+++ geometry/15973/15973_TwoBox.cpp
@@ -0,0 +1,16 @@
+## What is the problem?
+
+<https://www.acmicpc.net/problem/15973>
+
+## What Algorithm should I use?
+
+Geometry.
+
+## What was the key point and the hard part?
+
+Same with 2573
+
+## Where can I get more help, if I need it?
+
+You can contact me through email, which is wogur7496@gmail.com.
+Thank you for visiting this github!
