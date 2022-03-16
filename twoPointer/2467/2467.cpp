358d1439a59d97efbe9b05a9a2e7f44ba3e997f5
BulTTaeng <60182208+Choi-JaeHyeok-21500749@users.noreply.github.com>
2022-03-16 15:44:41 +0900

    Create 2467.cpp

---
+++ twoPointer/2467/2467.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int N;
+vector<int> v;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+
+    cin >> N;
+    int t;
+
+    for(int i =0; i<N; i++){
+        cin >> t;
+        v.push_back(t);
+    }
+
+    int start =0 ;
+    int end = v.size()-1;
+    int x , y;
+    long long sum = 2000000001;
+    long long diff = 0;
+    
+    while(start < end && end >=0){
+        diff = v[end] + v[start];
+
+        if(diff == 0){
+            x= start;
+            y= end;
+            break;
+        }
+        else{
+            if(diff < 0){
+                if(abs(sum) > abs(diff)){
+                    x = start;
+                    y = end;  
+                    sum = diff;
+                }
+                start++;
+            }
+            else{
+                if(abs(sum) > abs(diff)){
+                    x = start;
+                    y = end;  
+                    sum = diff;
+                }
+                end--;
+            }
+        }
+    }
+
+    cout << v[x] << " " << v[y];
+    return 0;
+}
