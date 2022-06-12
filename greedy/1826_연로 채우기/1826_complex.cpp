ba80267c8c56585c67742305bce974caf1475b01
BulTTaeng <60182208+Choi-JaeHyeok-21500749@users.noreply.github.com>
2022-06-12 21:45:18 +0900

    Create 1826_complex.cpp

---
+++ greedy/1826_연로 채우기/1826_complex.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int N , L ,P;
+int ans =0;
+
+struct compare_increasing{
+    bool operator()(pair<int , int > &p1 , pair<int , int > &p2){
+        if(p1.first == p2.first){
+            return p1.second > p2.second;
+        }
+        return p1.first > p2.first;
+    }
+};
+
+priority_queue<pair<int , int> , vector<pair<int , int>>  ,  compare_increasing> station;
+priority_queue<int> fuel;
+
+bool solve();
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+
+    cin >> N;
+    int dis , f;
+    for(int i =0; i<N; i++){
+        cin >> dis >> f;
+        station.push({dis , f});
+    }
+
+    cin >> L >> P;
+
+    if(solve()){
+        cout << ans;
+    }
+    else{
+        cout << -1;
+    }
+
+    return 0;
+}
+
+bool solve(){
+
+    int currloc = P;
+    bool re = false;
+
+    while(1){
+
+        //cout << currloc <<"\n";
+
+        if(currloc >= L){
+            re = true;
+            return re;
+        }
+
+        while(!station.empty() && station.top().first <= currloc ){ // fill up the fuels
+            fuel.push(station.top().second);
+            station.pop();
+        }
+
+        if(station.empty()){ // if we don't have source of fuel anymore.
+            while(!fuel.empty()){
+                currloc += fuel.top();
+                ans++;
+                if(currloc >= L){
+                    return true;
+                }
+                fuel.pop();
+            }
+
+            return false;
+        }
+
+        // if we have station left
+
+        if(fuel.empty()){ // if we don't have any fuel left
+            return false;
+        }
+        
+        while(!fuel.empty() && currloc < station.top().first){ // visit station to get the fuel to reach next station.
+            currloc += fuel.top();
+            ans++;
+            fuel.pop();
+        }
+
+    }
+
+    return re;
+}
