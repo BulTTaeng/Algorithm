ef42a9f4c492673345cb740dda7b272086269edc
BulTTaeng <60182208+BulTTaeng@users.noreply.github.com>
2023-11-25 17:31:26 +0900

    Create 8394.cpp

---
+++ DP/8394_악수/8394.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[10000001];
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cout.tie(nullptr);
+  cin.tie(nullptr);
+
+  int n;
+  cin>>n;
+
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for(int i =3 ; i<=n; i++){
+    dp[i] = (dp[i-1] + dp[i-2]) % 10;
+  }
+
+  cout << dp[n];
+  return 0;
+
+} 
