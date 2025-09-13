b54b32604e1231cc19cbb2da55baa1a4f9f1b7d4
BulTTaeng <60182208+BulTTaeng@users.noreply.github.com>
2025-09-13 18:33:55 +0900

    Create 11053.cpp

---
+++ Binary_Search/11053_가장 긴 증가하는 부분 수열/11053.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> inp;
+vector<int> ans;
+
+int N;
+
+void solve();
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> N;
+
+    int temp;
+    
+    for (int i =0; i<N; i++) {
+        cin >> temp;    
+        inp.push_back(temp);
+    }
+
+    solve();
+
+    cout << ans.size();
+    
+    return 0;
+}
+
+void solve() {
+
+    ans.push_back(inp[0]);
+    
+    for (int i =1; i<N; i++) {
+        if (ans.back() < inp[i]) {
+            ans.push_back(inp[i]);
+        } else {
+            int start = 0;
+            int end = ans.size()-1;
+            int mid = 0;
+            
+            while(start <= end) {
+                mid = (start+end)/2;
+                int curr = ans[mid];
+
+                if (curr < inp[i]) {
+                    start = mid+1;
+                } else {
+                    end = mid-1;
+                }
+            }
+
+            ans[start] = inp[i];
+        }
+    }
+}
