48edd57451b7e95f33151b4516a51d7c6ccfac40
BulTTaeng <60182208+BulTTaeng@users.noreply.github.com>
2025-09-27 12:25:48 +0900

    Create 1682.cpp

---
+++ graph/bfs,dfs/1682_돌리기/1682.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <set>
+#include <queue>
+
+using namespace std;
+
+string board = "";
+set<string> s;
+int ans = 0;
+queue<pair<int,string>> q;
+
+string A(string str);
+string B(string str);
+string C(string str);
+string D(string str);
+void bfs();
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    char c;
+    
+    for (int i =0; i<2; i++) {
+        for (int j =0; j<4; j ++) {
+            cin >> c;
+            board += c;
+        }
+    }
+
+    bfs();
+
+    cout << ans;
+    
+    return 0;
+}
+
+void bfs() {
+    q.push({0,"12345678"});
+    s.insert("12345678");
+
+    while(!q.empty()) {
+        int time = q.front().first;
+        string curr = q.front().second;
+        q.pop();
+
+        if (curr == board) {
+            ans = time;
+            break;
+        }
+
+        string n = A(curr);
+        
+        if(s.find(n) == s.end()) {
+            q.push({time+1, n});
+            s.insert(n);
+        }
+
+        n = B(curr);
+        
+        if(s.find(n) == s.end()) {
+            q.push({time+1, n});
+            s.insert(n);
+        }
+
+        n = C(curr);
+        
+        if(s.find(n) == s.end()) {
+            q.push({time+1, n});
+            s.insert(n);
+        }
+
+        n = D(curr);
+        
+        if(s.find(n) == s.end()) {
+            q.push({time+1, n});
+            s.insert(n);
+        }
+    }
+}
+
+string A(string str) {
+    string t = str;
+    for (int i =0; i<4; i++) {
+        int d = 7-i;
+        char temp = t[d];
+        t[d] = t[i];
+        t[i] = temp;
+    }
+    return t;
+}
+
+string B(string str) {
+    string t = str;
+    for (int i =3; i>=1; i--) {
+        t[i] = t[i-1];
+    }
+    t[0] = str[3];
+    for (int i =4; i<8; i++) {
+        t[i] = t[i+1];
+    }
+    t[7] = str[4];
+    return t;
+}
+
+string C(string str) {
+    string t = str;
+    t[1] = str[2];
+    t[2] = str[5];
+    t[5] = str[6];
+    t[6] = str[1];
+    return t;
+}
+
+string D(string str) {
+    string t = str;
+    t[0] = str[4];
+    t[4] = str[0];
+    return t;
+}
