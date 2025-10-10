aa2af4a5f95e7231190f11c0cd36b9ec1eb32a3b
BulTTaeng <60182208+BulTTaeng@users.noreply.github.com>
2025-10-11 00:00:57 +0900

    Update 15270.cpp

--- graph/bfs,dfs/15270_친구 팰린드롬/15270.cpp
+++ graph/bfs,dfs/15270_친구 팰린드롬/15270.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
-#include <algorithm>
 
 using namespace std;
 
@@ -36,23 +35,28 @@ int main() {
 }
 
 void dfs(int x, int couple) {
-    ans = max(ans, couple);
-    
-    if (x >= N) return;
+    if (x > N) {
+        ans = max(ans, couple);
+        return;
+    }
 
     if (visit[x]) {
-        dfs(x+1, couple); // 이미 커플이 된 친구면 pass
-    } else {
-        visit[x] = true;
-        for (int i =0; i<v[x].size(); i++) {
-            int nx = v[x][i];
-            if(!visit[nx]) {
-                visit[nx] = true; // 지금 친구(x)랑 커플 맺을 다른 친구 (nx) visit 처리
-                dfs(x+1, couple + 2);
-                visit[nx] = false;
-            }
+        dfs(x+1, couple);
+        return;
+    }
+    
+    visit[x] = true;
+    dfs(x+1, couple);
+    visit[x] = false;
+
+    for (int i =0; i<v[x].size(); i++) {
+        int nx = v[x][i];
+        if(!visit[nx]) {
+            visit[nx] = true;
+            visit[x] = true;
+            dfs(x+1, couple +2);
+            visit[nx] = false;
+            visit[x] = false;
         }
-        visit[x] = false;
-        dfs(x+1, couple); // 커플 맺지 않는 case 처리
     }
 }
