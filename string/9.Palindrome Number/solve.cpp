75cd8e5846d01bea9077d255cd40bad47a1a36dd
BulTTaeng <60182208+BulTTaeng@users.noreply.github.com>
2026-05-14 23:35:28 +0900

    Update solve.cpp

--- string/9.Palindrome Number/solve.cpp
+++ string/9.Palindrome Number/solve.cpp
@@ -3,10 +3,10 @@ public:
     bool isPalindrome(int x) {
         string str = to_string(x);
         int len = str.length();
-        int to = len/2;
+        int half = len/2;
 
 
-        for(int i =0; i<to; i++ ) {
+        for(int i =0; i<half; i++ ) {
             if (str[i] != str[len-1-i]) return false;
         }
         return true;
